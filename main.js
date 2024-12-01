1e5b3c3d6ad0b1251c1dae1c216bae5857090d5f
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2024-12-01 22:29:38 +0500

    contact section completed

--- main.js
+++ main.js
@@ -1,96 +1,15 @@
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelector("i");
-
-menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
-
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute(
-    "class",
-    isOpen ? "ri-close-line" : "ri-menu-3-line"
-  );
+document.addEventListener("DOMContentLoaded", () => {
+  const menuList = document.querySelector(".nav_links");
+  hamburger.addEventListener("click", () => {
+    if (menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "") {
+      menuList.style.maxHeight = "600px";
+      menuList.style.backgroundColor = "black";
+    } else {
+      menuList.style.maxHeight = "0px";
+    }
+  });
 });
 
-navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
-});
 
-const mixer = mixitup(".project__grid");
 
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
-};
-
-// header container
-ScrollReveal().reveal(".header__image img", {
-  ...scrollRevealOption,
-});
-
-ScrollReveal().reveal(".header__content h4", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-
-ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-
-ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-
-ScrollReveal().reveal(".header__content .btn", {
-  ...scrollRevealOption,
-  delay: 2000,
-});
-
-// about container
-ScrollReveal().reveal(".about__image img", {
-  ...scrollRevealOption,
-  origin: "left",
-});
-
-ScrollReveal().reveal(".about__content .section__header", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-
-ScrollReveal().reveal(".about__content p", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-
-ScrollReveal().reveal(".about__content h4", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-
-ScrollReveal().reveal(".about__btns", {
-  ...scrollRevealOption,
-  delay: 2000,
-});
-
-// service container
-ScrollReveal().reveal(".service__card", {
-  duration: 1000,
-  interval: 500,
-});
-
-// blog container
-ScrollReveal().reveal(".blog__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
+  
