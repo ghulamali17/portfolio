94245a3803a1741d9194e93b4a7d0a511d0d679d
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2024-12-04 17:51:18 +0500

    form validation added

--- main.js
+++ main.js
@@ -11,6 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// Form Validation
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    const inputs = this.querySelectorAll("input");
+    let flag = true;
 
+    inputs.forEach((input) => {
+      if (!input.value.trim()) {
+        flag = false;
+        input.style.border = "2px solid red";
+      } else {
+        input.style.border = "";
+      }
+    });
 
-  
+    if (!flag) {
+      event.preventDefault();
+      alert("Please fill in all fields");
+    }
+  });
